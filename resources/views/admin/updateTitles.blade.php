afddc1cf2a9b89e7acf31cba1395bdcba1def1e1
Gustavo Ramírez <gustavo81988@gmail.com>
2017-09-12 22:58:48 -0400

    modificada vista edit

--- resources/views/admin/updateTitles.blade.php
+++ resources/views/admin/updateTitles.blade.php
@@ -8,20 +8,43 @@
                 <div class="panel-heading">Titles</div>
 
                 <div class="panel-body">
-                    <form action="{{ route('titles.update', $titles->id) }}" role="form" method="post">
-                        {!! method_field('PUT') !!}
-                        {!! csrf_field() !!}
- 
-                        <input type="text" name="title" value="{{$titles->title}}">
-                        <input type="text" name="content" value="{{$titles->content}}">
-                        <input type="text" name="icon" value="{{$titles->icon}}">
- 
-                        <div class="form-group">
-                            <button class="form-control btn btn-info">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        
+                        <h3>Edit Title # {{$titles->id}}</h3>
+                        
+                        <form action="{{ route('titles.update', $titles->id) }}" role="form" method="post">
+                            {!! method_field('PUT') !!}
+                            {!! csrf_field() !!}
+                            <div class="form-group">
+                                <label for="title">Title</label>
+                                <input type="text" class="form-control" id="title" name="title" value="{{$titles->title}}">
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="title">Conten</label>
+                                <input type="text" class="form-control" id="content" name="content" value="{{$titles->content}}">
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="title">Icon</label>
+                                <select id="icon" name="icon" class="form-control form-control-lg">
+                                    <option value="fa fa-folder-open">Folder</option>
+                                    <option value="fa fa-asterisk">Asterisk</option>
+                                    <option value="fa fa-bookmark">Bookmark</option>
+                                    <option value="fa fa-calendar-check-o">Calendar</option>
+                                    <option value="fa fa-cubes">Cubes</option>
+                                    <option value="fa fa-cogs">Cogs</option>
+                                    <option value="fa fa-exclamation-triangle">Triangle</option>
+                                    <option value="fa fa-clipboard">Clipboard</option>
+                                </select>
+                            </div>
+                            
+                            <button class="btn btn-primary">
                                 Save
                             </button>
-                        </div>
-                    </form>
+                            
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
