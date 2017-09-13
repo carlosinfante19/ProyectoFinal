66f4d51de012a1ec455004f24295ab779aa0b47c
Gustavo Ramírez <gustavo81988@gmail.com>
2017-09-12 20:09:36 -0400

    avance

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,19 @@
+<header class="site-header">
+    <div class="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <p>Lorem Ipsum Dolor Sit Amet</p>
+                </div>
+                <div class="col-sm-6">
+                    <ul class="list-inline pull-right">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#"><i class="fa fa-envelope-o"></i></a></li>
+                        <li><a href="tel:+902222222"><i class="fa fa-phone"></i> +90 222 2222</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
