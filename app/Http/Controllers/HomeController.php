780e61c835aea51434e38948a604a3239472a8ff
Gustavo Ramírez <gustavo81988@gmail.com>
2017-09-14 10:32:02 -0400

    corregido rutas auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,15 +10,7 @@ use App\Blog;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    
 
     /**
      * Show the application dashboard.
